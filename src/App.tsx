666c30eb41ffe285bf6292011590446504fa4c30
akaydilaraa <147245101+akaydilaraa@users.noreply.github.com>
2026-02-24 23:10:32 +0300

    chore: initial project setup with Vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Dilara [AKAY]</p>
+      <p>Ogrenci No: [225542007]</p>
+    </div>
+  )
+}
+
+export default App
